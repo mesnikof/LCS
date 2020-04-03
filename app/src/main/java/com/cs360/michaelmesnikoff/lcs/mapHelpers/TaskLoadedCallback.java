3d97679163215277c2ee6e2e0f007127e71a2d13
mesnikof <mesikof@gsinet.net>
2020-04-03 14:28:41 -0400

    Initial commit

---
+++ app/src/main/java/com/cs360/michaelmesnikoff/lcs/mapHelpers/TaskLoadedCallback.java
@@ -0,0 +1,16 @@
+package com.cs360.michaelmesnikoff.lcs.mapHelpers;
+
+/**
+ * Created by mp on 4/2/20.
+ */
+
+/**
+ * Created by Vishal on 10/20/2018.
+ */
+
+/*
+ * This class simply contains a callback responder.
+ */
+public interface TaskLoadedCallback {
+    void onTaskDone(Object... values);
+}
