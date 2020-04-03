332334ef091fc2ff08b2803d8529f5441ba2352d
Michael Mesnikoff <mesbikof@gsinet.net>
2020-04-03 16:08:05 -0400

    Initial commit

--- app/src/main/java/com/cs360/michaelmesnikoff/lcs/LoginActivity.java
+++ app/src/main/java/com/cs360/michaelmesnikoff/lcs/LoginActivity.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
