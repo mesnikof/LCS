fd4da4dffd1fd73479a29a4e47184d8dd9225a47
Michael Mesnikoff <mesbikof@gsinet.net>
2020-04-21 18:43:06 -0400

    Final Project Initial Commit

---
+++ app/src/main/java/com/cs360/michaelmesnikoff/lcs/CreateNewUser.java
@@ -0,0 +1,21 @@
+package com.cs360.michaelmesnikoff.lcs;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by mp on 4/21/20.
+ */
+
+public class CreateNewUser {
+
+    private Context context;
+
+    public CreateNewUser(Context c) {
+
+        context = c;
+
+        Toast.makeText(c, "In the class...", Toast.LENGTH_LONG).show();
+
+    }
+}
